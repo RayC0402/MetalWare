9be8b3fbd7eb0a9334b1a9030a37c34461f4814d
RayC <RayC@DESKTOP-90RF1RC>
2022-11-09 15:38:46 +0800

    WTF

--- MetalWare/Program.cs
+++ MetalWare/Program.cs
@@ -15,25 +15,25 @@ namespace MetalWare
         {
             List<itemType> list = new List<itemType>();
             XmlDocument xd = new XmlDocument();
-            xd.LoadXml("./ItemNewMetalWare.xml");
-            // XmlNodeList xnl = xd.SelectNodes(@"NewMetalWare/LevelDistributionList/LevelDistribution");
-            // foreach(XmlNode itemtypes in xnl)
-            // {
-            //     var metalitemType = new itemType()
-            //     {
-            //         ItemTypeId = Convert.ToInt32(itemtypes.Attributes["ItemTypeId"].Value),
-            //         RateRank3 = Convert.ToInt32(itemtypes.Attributes["RateRank3"].Value),
-            //         RateRank2 = Convert.ToInt32(itemtypes.Attributes["RateRank2"].Value),
-            //         RateRank1 = Convert.ToInt32(itemtypes.Attributes["RateRank1"].Value),
-            //         RateAbility1 = Convert.ToInt32(itemtypes.Attributes["RateAbility1"].Value),
-            //         RateAbility2 = Convert.ToInt32(itemtypes.Attributes["RateAbility2"].Value),
-            //         RateAbility3 = Convert.ToInt32(itemtypes.Attributes["RateAbility3"].Value),
-            //     };
-            //     list.Add(metalitemType);
-            // }
-            // var options = new JsonSerializerOptions { WriteIndented = true };
-            // string jsonType = JsonSerializer.Serialize(list, options);
-            // File.WriteAllText("./MetalWareItemType.txt", jsonType, Encoding.UTF8);
+            xd.Load("./ItemNewMetalWare.xml");
+            XmlNodeList xnl = xd.SelectNodes(@"NewMetalWare/LevelDistributionList/LevelDistribution");
+            foreach(XmlNode itemtypes in xnl)
+            {
+                var metalitemType = new itemType()
+                {
+                    ItemTypeId = Convert.ToInt32(itemtypes.Attributes["ItemTypeId"].Value),
+                    RateRank3 = Convert.ToInt32(itemtypes.Attributes["RateRank3"].Value),
+                    RateRank2 = Convert.ToInt32(itemtypes.Attributes["RateRank2"].Value),
+                    RateRank1 = Convert.ToInt32(itemtypes.Attributes["RateRank1"].Value),
+                    RateAbility1 = Convert.ToInt32(itemtypes.Attributes["RateAbility1"].Value),
+                    RateAbility2 = Convert.ToInt32(itemtypes.Attributes["RateAbility2"].Value),
+                    RateAbility3 = Convert.ToInt32(itemtypes.Attributes["RateAbility3"].Value),
+                };
+                list.Add(metalitemType);
+            }
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonType = JsonSerializer.Serialize(list, options);
+            File.WriteAllText("./MetalWareItemType.txt", jsonType, Encoding.UTF8);
             Console.WriteLine("done");
         }
     }
